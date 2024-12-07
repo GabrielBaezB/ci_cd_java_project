74aefbfe52189e0fd277cc6126c9777f50190e11
GabrielBaezB <gabooo@live.com>
2024-12-07 12:52:45 -0300

    se actualiza main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,12 @@ terraform {
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
+
+resource "docker_container" "app_container" {
+  name  = "ci_cd_java_project_container"
+  image = docker_image.app_image.latest
+  ports {
+    internal = 8080
+    external = 8080
+  }
+}
