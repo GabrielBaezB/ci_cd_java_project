6edb20ec3dbd5ac3516a7484bdcab798fea6af36
GabrielBaezB <gabooo@live.com>
2024-12-07 09:51:12 -0300

    Primer commit

---
+++ src/main/java/com/certificacion/ci_cd_java_project/CiCdJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.certificacion.ci_cd_java_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdJavaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/certificacion/ci_cd_java_project/CiCdJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certificacion.ci_cd_java_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
