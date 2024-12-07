98398fc5ce1715fc07e56dbfe5eb7fc261dd42fc
GabrielBaezB <gabooo@live.com>
2024-12-07 12:56:09 -0300

    se actualiza main.tf -2

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,9 +11,18 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
+# Construcción de la imagen Docker
+resource "docker_image" "app_image" {
+  name = "ci_cd_java_project"
+  build {
+    context = "../"  # El contexto debe ser el directorio donde está tu Dockerfile
+  }
+}
+
+# Creación del contenedor Docker
 resource "docker_container" "app_container" {
   name  = "ci_cd_java_project_container"
-  image = docker_image.app_image.latest
+  image = docker_image.app_image.latest  # Aquí se hace referencia a la imagen construida
   ports {
     internal = 8080
     external = 8080
